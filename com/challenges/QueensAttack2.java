ced8b0f9d09501ccfe894aee459863215e405b4d
jaekyungkim <dev.jaekyungkim@gmail.com>
2022-11-28 12:28:48 +0900

    Queen's attack 작업 시작

---
+++ com/challenges/QueensAttack2.java
@@ -0,0 +1,22 @@
+package com.challenges;
+
+import java.util.List;
+
+public class QueensAttack2 {
+    /*
+     * Complete the 'queensAttack' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER r_q
+     *  4. INTEGER c_q
+     *  5. 2D_INTEGER_ARRAY obstacles
+     */
+
+    public static int queensAttack(int n, int k, int r_q, int c_q, List<List<Integer>> obstacles) {
+        // Write your code here
+        return 0;
+    }
+}
